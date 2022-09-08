e5752bf84556214ad206a56ba7d69a0022fa7d61
Cliff Hsu <cliff60714@gmail.com>
2022-09-08 20:23:03 +0800

    add read file

--- pr.py
+++ pr.py
@@ -1,4 +1,12 @@
 product = []
+with open ('product.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        product.append([name,price])
+
+print(product)    
 
 while True:
     name = input('請輸入商品名稱:')
@@ -10,3 +18,8 @@ print(product)
 
 for p in product:
     print(p[0], "的價格是", p[1])
+
+with open('product.csv' ,'w', encoding = 'utf-8') as f :
+    f.write('商品,價格\n')
+    for p in product:
+        f.write(p[0] + ',' + p[1] + '\n') 
